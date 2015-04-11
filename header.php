80134a872c217b8769a919d875a45c92ca7d115b
Carlos Centeno <carlos@thinkinvictus.com>
2015-04-10 20:58:28 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html lang="en">
+    <head>
+        <title>Carlos Centeno</title>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+        <header role="banner">
+            <h1>Carlos Centeno</h1>
+        </header>
+        
+        <main role="main">
+              
---
+++ index.php
@@ -0,0 +1,123 @@
+<?php include 'header.php'; ?>
+
+<div class="work--nav work--nav__bar">
+    <nav role="navigation">
+        <a href="#project-1" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-2" class="active project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+        <a href="#project-3" class="project--info__container">
+            <span class="tab--circle"></span>
+            <span class="project--info project--info__nav">
+                <span class="project--title project--title__nav">Title of work</span>
+                <span class="project--type project--type__nav">Website</span>
+            </span>
+        </a>
+    </nav>
+</div>
+<section class="work work--list work--list__main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-20">
+                <article id="project-1" class="project--group project--group__large">
+                    <a href="#">
+                        <figure class="figure--container figure--container__circle">
+                            <img src="http://placehold.it/900x900">
+                        </figure>
+                        <div class="project--info">
+                            <h2 class="project--title">Title of Work</h2>
+                            <p class="project--desc">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </a>
+                </article>
+                
+                <article id="project-2" class="project--group project--group__large">
+                    <a href="#">
+                        <figure class="figure--container figure--container__circle">
+                            <img src="http://placehold.it/900x900">
+                        </figure>
+                        <div class="project--info">
+                            <h2 class="project--title">Title of Work</h2>
+                            <p class="project--desc">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </a>
+                </article>
+                
+                <article id="project-3" class="project--group project--group__large">
+                    <a href="#">
+                        <figure class="figure--container figure--container__circle">
+                            <img src="http://placehold.it/900x900">
+                        </figure>
+                        <div class="project--info">
+                            <h2 class="project--title">Title of Work</h2>
+                            <p class="project--desc">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </a>
+                </article>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'footer.php'; ?>
